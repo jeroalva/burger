0e79db20c529f174d0f8c6cbd99ee2299d92f4c0
Jero Alvarez <jeroalva@gmail.com>
2019-12-12 10:56:10 -0600

    Agregue el codigo para que use las variables de BD de Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require("mysql");
 require("dotenv").config();
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
     host: "localhost",
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: "burgers"
 });
+}
 
 connection.connect(function(err){
     if (err){
